4e0808d24b334f0eb1dee7dbf0399b57ac54639a
Gitansh Raj Satija <55681531+gitongithub@users.noreply.github.com>
2022-12-23 02:15:28 +0530

    added code

--- app.py
+++ app.py
@@ -3,6 +3,9 @@
 
 from flask import Flask, request, render_template
 import mysql.connector
+
+from createImdbDB import mycursor
+
 # mydb = mysql.connector.connect(
 #     host="localhost",
 #     database="iia_project",
@@ -23,7 +26,7 @@ def index():
 @app.route('/query/<string:queryNum>')
 def query(queryNum):
 
-    queryParams = request.args.get('queryParam')  # movie name etc.
+    queryParams = request.args.get('movie_name')  # movie name etc.
     # get query from queryNum
    # this corresponds to the queries present in the index page
     queries = [
@@ -44,15 +47,15 @@ def query(queryNum):
     print(queryNum)
     # query = "SELECT * FROM TMDBMovie WHERE title LIKE '%{}%'".format(queryParam)
 
-    # mycursor.execute(query["query"])
-    # data = mycursor.fetchall()
+    mycursor.execute(query["query"])
+    data = mycursor.fetchall()
     # run query with params based on query number and then return data
-    data = [
-        ["title", "original_title", "release_date", "runtime", "is_adult", "imdb_link", "overview",
-            "popularity", "poster_path", "collection_belongs_to", "revenue", "budget", "tagline", "homepage_link"],
-        ["title", "original_title", "release_date", "runtime", "is_adult", "imdb_link", "overview",
-            "popularity", "poster_path", "collection_belongs_to", "revenue", "budget", "tagline", "homepage_link"],
-    ]  # dummy data and columns because my sql db stopped working (mac issue)
+    # data = [
+    #     ["title", "original_title", "release_date", "runtime", "is_adult", "imdb_link", "overview",
+    #         "popularity", "poster_path", "collection_belongs_to", "revenue", "budget", "tagline", "homepage_link"],
+    #     ["title", "original_title", "release_date", "runtime", "is_adult", "imdb_link", "overview",
+    #         "popularity", "poster_path", "collection_belongs_to", "revenue", "budget", "tagline", "homepage_link"],
+    # ]  # dummy data and columns because my sql db stopped working (mac issue)
     query_columns = ["title", "original_title", "release_date", "runtime", "is_adult", "imdb_link", "overview",
                      "popularity", "poster_path", "collection_belongs_to", "revenue", "budget", "tagline", "homepage_link"]
     return render_template('query.html', data=data, columns=query_columns, query_name=query["name"])
--- createImdbDB.py
+++ createImdbDB.py
@@ -1,24 +1,24 @@
 # import pandas as pd
 # import os
-
+#
 # DATA_DIR = "data/tmdb/"
-
+#
 # # for all files in data_dir convert to csv
 # for file in os.listdir(DATA_DIR):
 #     if file.endswith(".xlsx"):
 #         df = pd.read_excel(DATA_DIR + file)
 #         df.to_csv(DATA_DIR + file[:-5] + ".tsv", index=False, sep="\t")
-
-
+#
+#
 import csv
 import mysql.connector
 from datetime import datetime
 
 mydb = mysql.connector.connect(
     host="localhost",
-    database="iia_project"
-    # user="root",
-    # password="rootroot"
+    database="iia_project",
+    user="root",
+    password="123456"
 )
 
 mycursor = mydb.cursor()
@@ -29,44 +29,97 @@ sql = "INSERT INTO TMDBMovie(title, original_title, release_date, runtime, is_ad
 
 imdbSQL = "INSERT INTO IMDBMovie(primary_title, original_title, release_date, runtime, is_adult, imdb_link, genres) VALUES (%s, %s, %s, %s, %s, %s, %s)"
 
+sqlPerson = "INSERT INTO TMDBPerson(id, name, birthday, deathday, alias_name, gender, biography, profile_path, popularity, place_of_birth, homepage, home_country, imdb_link) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+imdbPerson = "INSERT INTO IMDBPerson(id, name, birthday, deathday, primary_profession, known_for, imdb_link) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+
 
 # insert imdb rows
-count = 0
-with open('data/imdb/titleBasics.csv', 'r') as f:
-    reader = csv.reader(f, delimiter=',')
-    for row in reader:
-        if count == 0:
-            count += 1
-            continue
-        if count >= 1000:
-            break
-        count += 1
-        id = row[0]
-        imdb_id = row[1]
-        primaryTitle = row[3]
-        originalTitle = row[4]
-        isAdult = row[5]
-        startYear = row[6]
-        endYear = row[7]
-        runtimeMinutes = row[8]
-        genres = row[9]
-        # convert start year to date type for sql
-        if startYear != '\\N':
-            startYear = datetime.strptime(startYear, '%Y')
-        # convert runtime to int
-        if runtimeMinutes != '\\N':
-            runtimeMinutes = int(runtimeMinutes)
-        else:
-            runtimeMinutes = None
+# count = 0
+# with open('data/imdb/titleBasics.csv', 'r') as f:
+#     reader = csv.reader(f, delimiter=',')
+#     for row in reader:
+#         if count == 0:
+#             count += 1
+#             continue
+#         if count >= 1000:
+#             break
+#         count += 1
+#         id = row[0]
+#         imdb_id = row[1]
+#         primaryTitle = row[3]
+#         originalTitle = row[4]
+#         isAdult = row[5]
+#         startYear = row[6]
+#         endYear = row[7]
+#         runtimeMinutes = row[8]
+#         genres = row[9]
+#         # convert start year to date type for sql
+#         if startYear != '\\N':
+#             startYear = datetime.strptime(startYear, '%Y')
+#         # convert runtime to int
+#         if runtimeMinutes != '\\N':
+#             runtimeMinutes = int(runtimeMinutes)
+#         else:
+#             runtimeMinutes = None
+#
+#         # print(id, primaryTitle, originalTitle, isAdult, startYear, endYear, runtimeMinutes, genres)
+#         vals = (primaryTitle, originalTitle, startYear, runtimeMinutes,
+#                 isAdult, imdb_id, genres)
+#         mycursor.execute(imdbSQL, vals)
+#         mydb.commit()
+#
+# count = 0
+# with open('data/tmdb/movie.tsv', 'r',encoding="utf8") as f:
+#     reader = csv.reader(f, delimiter='\t')
+#     # for each line in the file
+#     for row in reader:
+#
+#         if count == 0:
+#             count += 1
+#             continue
+#         if count >= 1000:
+#             break
+#         count += 1
+#         # get the movie id
+#         id = row[0]
+#         film_id = row[1]
+#         title = row[2]
+#         isAdult = row[3]
+#         backdrop_path = row[4]
+#         budget = row[5]
+#         homepage = row[6]
+#         imdb_id = row[7]
+#         original_language = row[8]
+#         original_title = row[9]
+#         overview = row[10]
+#         popularity = row[11]
+#         poster_path = row[12]
+#         release_date = row[13]
+#         revenue = row[14]
+#         runtime = row[15]
+#         vote_average = row[16]
+#         vote_count = row[17]
+#         status = row[18]
+#         tagline = row[19]
+#         collection_id = row[20]
+#         isAdult = bool(isAdult)
+#         if isAdult:
+#             isAdult = 1
+#         else:
+#             isAdult = 0
+#         try:
+#             vals = (title, original_title, release_date, runtime, isAdult, imdb_id, overview,
+#                     popularity, poster_path, collection_id, revenue, budget, tagline, homepage)
+#
+#             mycursor.execute(sql, vals)
+#             mydb.commit()
+#         except Exception as e:
+#             print(e)
+#             print(vals)
 
-        # print(id, primaryTitle, originalTitle, isAdult, startYear, endYear, runtimeMinutes, genres)
-        vals = (primaryTitle, originalTitle, startYear, runtimeMinutes,
-                isAdult, imdb_id, genres)
-        mycursor.execute(imdbSQL, vals)
-        mydb.commit()
-
 count = 0
-with open('data/tmdb/movie.tsv', 'r') as f:
+with open('data/tmdb/PERSON.tsv', 'r',encoding='utf8') as f:
     reader = csv.reader(f, delimiter='\t')
     # for each line in the file
     for row in reader:
@@ -78,37 +131,71 @@ with open('data/tmdb/movie.tsv', 'r') as f:
             break
         count += 1
         # get the movie id
-        id = row[0]
-        film_id = row[1]
-        title = row[2]
-        isAdult = row[3]
-        backdrop_path = row[4]
-        budget = row[5]
-        homepage = row[6]
-        imdb_id = row[7]
-        original_language = row[8]
-        original_title = row[9]
-        overview = row[10]
-        popularity = row[11]
-        poster_path = row[12]
-        release_date = row[13]
-        revenue = row[14]
-        runtime = row[15]
-        vote_average = row[16]
-        vote_count = row[17]
-        status = row[18]
-        tagline = row[19]
-        collection_id = row[20]
-        isAdult = bool(isAdult)
-        if isAdult:
-            isAdult = 1
-        else:
-            isAdult = 0
-        try:
-            vals = (title, original_title, release_date, runtime, isAdult, imdb_id, overview,
-                    popularity, poster_path, collection_id, revenue, budget, tagline, homepage)
 
-            mycursor.execute(sql, vals)
+        id = row[1]
+        name = row[2]
+        birthday = row[5]
+        deathday = row[7]
+        alias_name = row[6]
+        gender = row[8]
+        biography = row[9]
+        profile_path = row[10]
+        popularity = row[11]
+        place_of_birth = row[12]
+        homepage = row[15]
+        home_country = row[17]
+        imdb_link = row[14]
+        if birthday=='':
+            birthday=None
+        if deathday=='':
+            deathday=None
+        try:
+            vals = (id, name, birthday, deathday, alias_name, gender, biography,
+                    profile_path, popularity, place_of_birth, homepage, home_country, imdb_link)
+
+            mycursor.execute(imdbPerson, vals)
+            mydb.commit()
+        except Exception as e:
+            print(e)
+            print(vals)
+
+
+count = 0
+with open('data/tmdb/PERSON.tsv', 'r',encoding='utf8') as f:
+    reader = csv.reader(f, delimiter='\t')
+    # for each line in the file
+    for row in reader:
+
+        if count == 0:
+            count += 1
+            continue
+        if count >= 1000:
+            break
+        count += 1
+        # get the movie id
+
+        id = row[1]
+        name = row[2]
+        birthday = row[5]
+        deathday = row[7]
+        alias_name = row[6]
+        gender = row[8]
+        biography = row[9]
+        profile_path = row[10]
+        popularity = row[11]
+        place_of_birth = row[12]
+        homepage = row[15]
+        home_country = row[17]
+        imdb_link = row[14]
+        if birthday=='':
+            birthday=None
+        if deathday=='':
+            deathday=None
+        try:
+            vals = (id, name, birthday, deathday, alias_name, gender, biography,
+                    profile_path, popularity, place_of_birth, homepage, home_country, imdb_link)
+
+            mycursor.execute(sqlPerson, vals)
             mydb.commit()
         except Exception as e:
             print(e)
