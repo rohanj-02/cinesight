d00ee3d9308ea9cd1d18e3936055a9ff1003260e
Gitansh Raj Satija <55681531+gitongithub@users.noreply.github.com>
2022-12-23 07:26:48 +0530

    added code

--- app.py
+++ app.py
@@ -23,19 +23,34 @@ def index():
 @app.route('/query/<string:queryNum>')
 def query(queryNum):
 
-    queryParams = request.args.get('queryParam')  # movie name etc.
+    queryParams = [request.args.get('genre'),'','',request.args.get('keyword'),'']  # movie name etc.
     # get query from queryNum
    # this corresponds to the queries present in the index page
     queries = [
         {
             # dummy query
-            "query": "SELECT * FROM TMDBMovie WHERE title LIKE '%{}%'".format(queryParams),
-            "name": "Recommend movies by title"
+            "query": "select title, release_date, runtime, overview, popularity, genres,rating,ratings from Movie where runtime<100 and (LOWER(Movie.genres)) LIKE '%{}%' ORDER BY runtime".format(queryParams[0]),
+            "name": "Check Out Short Movies of your favourite genre"
         },
         {
             # dummy query
-            "query": "SELECT * FROM TMDBMovie WHERE original_title LIKE '%{}%'".format(queryParams),
-            "name": "Show all movies with original title"
+            "query": 'select title, release_date, runtime, overview, popularity, genres,rating,ratings from Movie where popularity>50 and rating>8.2 and ratings>4',
+            "name": "Query: Show all movies that are very famous across all platforms"
+        },
+        {
+            # dummy query
+            "query": "select year(release_date), AVG(rating) 'IMDB Average Ratings', AVG(ratings) 'Movielens Average Ratings', AVG(popularity) from Movie group by (year(release_date))  ORDER BY year(release_date) desc",
+            "name": "Query: Yearwise summary of ratings across different years"
+        },
+        {
+            # dummy query
+            "query": "SELECT title, release_date, runtime, overview, popularity, genres FROM Movie WHERE LOWER(Movie.title) LIKE '%{}%' or LOWER(Movie.overview) LIKE '%{}%' or LOWER(Movie.genres) LIKE '%{}%';".format(queryParams[3],queryParams[3],queryParams[3]),
+            "name": "Query: Search movies using a keyword based filter"
+        },
+        {
+            # dummy query
+            "query": "select title, release_date, runtime, overview, popularity, genres,rating,ratings from Movie where revenue-budget<0 and rating>7; ",
+            "name": "Popular Movies that made a loss in the theatres"
         }
     ]
     query = queries[int(queryNum)]
@@ -44,15 +59,14 @@ def query(queryNum):
     print(queryNum)
     # query = "SELECT * FROM TMDBMovie WHERE title LIKE '%{}%'".format(queryParam)
 
-    # mycursor.execute(query["query"])
-    # data = mycursor.fetchall()
+    mycursor.execute(query["query"])
+    data = mycursor.fetchall()
     # run query with params based on query number and then return data
-    data = [
-        ["title", "original_title", "release_date", "runtime", "is_adult", "imdb_link", "overview",
-            "popularity", "poster_path", "collection_belongs_to", "revenue", "budget", "tagline", "homepage_link"],
-        ["title", "original_title", "release_date", "runtime", "is_adult", "imdb_link", "overview",
-            "popularity", "poster_path", "collection_belongs_to", "revenue", "budget", "tagline", "homepage_link"],
-    ]  # dummy data and columns because my sql db stopped working (mac issue)
-    query_columns = ["title", "original_title", "release_date", "runtime", "is_adult", "imdb_link", "overview",
-                     "popularity", "poster_path", "collection_belongs_to", "revenue", "budget", "tagline", "homepage_link"]
-    return render_template('query.html', data=data, columns=query_columns, query_name=query["name"])
+    # data = [
+    #     ["title", "original_title", "release_date", "runtime", "is_adult", "imdb_link", "overview",
+    #         "popularity", "poster_path", "collection_belongs_to", "revenue", "budget", "tagline", "homepage_link"],
+    #     ["title", "original_title", "release_date", "runtime", "is_adult", "imdb_link", "overview",
+    #         "popularity", "poster_path", "collection_belongs_to", "revenue", "budget", "tagline", "homepage_link"],
+    # ]  # dummy data and columns because my sql db stopped working (mac issue)
+    query_columns = [['title', 'release_date', 'runtime', 'overview', 'popularity', 'genres','rating','ratings'],['title', 'release_date', 'runtime', 'overview', 'popularity', 'genres','rating','ratings'],['year', 'IMDB Average Ratings', 'Movielens Average Ratings','popularity'],['title', 'release_date', 'runtime', 'overview', 'popularity', 'genres','rating','ratings'],['title', 'release_date', 'runtime', 'overview', 'popularity', 'genres','rating','ratings']]
+    return render_template('query.html', data=data, columns=query_columns[int(queryNum)], query_name=query["name"])
